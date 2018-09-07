dd4fc2b01b9fdbd11b636f9c2e501da9271d3d23
Bole <jelenabole@gmail.com>
2018-09-07 17:19:43 +0200

    added snackbar for notifications

--- app/src/main/java/hr/tvz/trackmydog/services/NotificationService.java
+++ app/src/main/java/hr/tvz/trackmydog/services/NotificationService.java
@@ -1,9 +1,16 @@
 package hr.tvz.trackmydog.services;
 
+import android.app.Activity;
+import android.app.Notification;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -13,6 +20,10 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import hr.tvz.trackmydog.R;
 import hr.tvz.trackmydog.localDB.Token;
 
 // TODO - token is same for multiple users on the same device
@@ -100,6 +111,7 @@ public class NotificationService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
         Log.d(TAG, " *** Message received: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
@@ -113,12 +125,23 @@ public class NotificationService extends FirebaseMessagingService {
             Log.d(TAG, " *** Message Notification Body: " + remoteMessage.getNotification().getBody());
 
             // show message to user:
-            showLongToast(this, remoteMessage.getNotification().getBody());
+            showSnack(remoteMessage.getNotification().getBody());
+            // showLongToast(this, remoteMessage.getNotification().getBody());
         }
     }
 
+    private void showNotification(RemoteMessage remoteMessage, Context context) {
+        Notification notification = new NotificationCompat.Builder(this, "tracking channel")
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(123, notification);
+    }
+
     // send toast messages from anywhere
-    public static void showLongToast(final Context context, final String message) {
+    private void showLongToast(final Context context, final String message) {
         // can't toast on a thread that has not called Looper.prepare()
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
@@ -128,6 +151,60 @@ public class NotificationService extends FirebaseMessagingService {
                 // Toast.makeText(MyApplication.getContext().getApplicationContext(), message, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    // send snack messages from anywhere
+    private void showSnack(final String message) {
+        final Activity activity = getActivity();
+        if (activity == null) {
+            Log.e(TAG, "show snack = can't get activity");
+            return;
+        }
+
+        Snackbar snackbar = Snackbar.make(activity.getWindow().getDecorView().getRootView()
+                        .findViewById(android.R.id.content),
+                message, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("REMOVE", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "snackbar should be removed");
+            }
+        });
+        // change action color:
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+        snackbar.show();
+    }
+
+    // TODO - get current activity
+    public static Activity getActivity() {
+        try {
+            Class activityThreadClass = Class.forName("android.app.ActivityThread");
+            Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+            Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+            activitiesField.setAccessible(true);
+
+            Map<Object, Object> activities = (Map<Object, Object>) activitiesField.get(activityThread);
+            if (activities == null)
+                return null;
+
+            for (Object activityRecord : activities.values()) {
+                Class activityRecordClass = activityRecord.getClass();
+                Field pausedField = activityRecordClass.getDeclaredField("paused");
+                pausedField.setAccessible(true);
+                if (!pausedField.getBoolean(activityRecord)) {
+                    Field activityField = activityRecordClass.getDeclaredField("activity");
+                    activityField.setAccessible(true);
+                    Activity activity = (Activity) activityField.get(activityRecord);
+                    return activity;
+                }
+            }
+
+            return null;
+        } catch (Exception ex) {
+            // TODO - handle exceptions - check code
+            Log.d(TAG, "Error while getting current activity");
+            return null;
+        }
     }
 
 }
